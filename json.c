5b9b940cb92580e79c0a5702aab4d16047752bb2
charcoalSkies <jun48470@gmail.com>
2023-05-24 17:26:43 +0900

    docs : 주석 설명 추가

--- json.c
+++ json.c
@@ -1,56 +1,69 @@
-/*Copyright*/
 #include "./json.h"
 
-sensors json_parser(json_object *data) 
-{
-    json_object* led_data = json_object_object_get(data, "LED");
+// json_object를 파싱하여 센서 데이터를 추출하는 함수
+sensors json_parser(json_object* data) {
+  // LED 데이터 추출
+  json_object* led_data = json_object_object_get(data, "LED");
 
-    json_object* buzzer_data = json_object_object_get(data, "BUZZER");
-    json_object* buzzer_usage = json_object_object_get(buzzer_data, "usage");
-    json_object* buzzer_hertz = json_object_object_get(buzzer_data, "hertz");
+  // BUZZER 데이터 추출
+  json_object* buzzer_data = json_object_object_get(data, "BUZZER");
+  json_object* buzzer_usage = json_object_object_get(buzzer_data, "usage");
+  json_object* buzzer_hertz = json_object_object_get(buzzer_data, "hertz");
 
-    json_object* motor_data = json_object_object_get(data, "MOTOR");
-    json_object* motor_usage = json_object_object_get(motor_data, "usage");
-    json_object* motor_angle = json_object_object_get(motor_data, "angle");
+  // MOTOR 데이터 추출
+  json_object* motor_data = json_object_object_get(data, "MOTOR");
+  json_object* motor_usage = json_object_object_get(motor_data, "usage");
+  json_object* motor_angle = json_object_object_get(motor_data, "angle");
 
-    json_object* lcd_data = json_object_object_get(data, "LCD");
-    json_object* lcd_usage = json_object_object_get(lcd_data, "usage");
-    json_object* lcd_text = json_object_object_get(lcd_data, "text");
+  // LCD 데이터 추출
+  json_object* lcd_data = json_object_object_get(data, "LCD");
+  json_object* lcd_usage = json_object_object_get(lcd_data, "usage");
+  json_object* lcd_text = json_object_object_get(lcd_data, "text");
 
-    json_object* sound_data = json_object_object_get(data, "SOUND");
-    json_object* light_data = json_object_object_get(data, "LIGHT");
+  // SOUND, LIGHT 데이터 추출
+  json_object* sound_data = json_object_object_get(data, "SOUND");
+  json_object* light_data = json_object_object_get(data, "LIGHT");
 
-    sensors sensor = {0, };
+  // 추출한 데이터를 저장할 구조체 변수 생성
+  sensors sensor = {
+      0,
+  };
 
-    sensor.led = json_object_get_string(led_data);
-    
-    sensor.buzzer_usage = json_object_get_string(buzzer_usage);
-    sensor.buzzer_hertz = json_object_get_string(buzzer_hertz);
-    
-    sensor.motor_usage = json_object_get_string(motor_usage);
-    sensor.motor_angle = json_object_get_string(motor_angle);
+  // LED 데이터 저장
+  sensor.led = json_object_get_string(led_data);
 
-    sensor.lcd_usage = json_object_get_string(lcd_usage);
-    sensor.lcd_text = json_object_get_string(lcd_text);
-    
-    sensor.sound = json_object_get_string(sound_data);
-    sensor.light = json_object_get_string(light_data);
+  // BUZZER 데이터 저장
+  sensor.buzzer_usage = json_object_get_string(buzzer_usage);
+  sensor.buzzer_hertz = json_object_get_string(buzzer_hertz);
 
-    return sensor;
+  // MOTOR 데이터 저장
+  sensor.motor_usage = json_object_get_string(motor_usage);
+  sensor.motor_angle = json_object_get_string(motor_angle);
+
+  // LCD 데이터 저장
+  sensor.lcd_usage = json_object_get_string(lcd_usage);
+  sensor.lcd_text = json_object_get_string(lcd_text);
+
+  // SOUND, LIGHT 데이터 저장
+  sensor.sound = json_object_get_string(sound_data);
+  sensor.light = json_object_get_string(light_data);
+
+  // 추출한 센서 데이터를 반환
+  return sensor;
 }
 
-char* str_slicing(char* s, int start, int end) 
-{
-    // 시작과 끝 인덱스를 이용하여 문자열의 크기를 계산
-    int str_size = end - start;
-    // 슬라이싱된 문자열을 저장할 포인터 변수를 동적 할당
-    char* slice_str = (char*)malloc(sizeof(char) * str_size);
+// 문자열 슬라이싱 함수
+char* str_slicing(char* s, int start, int end) {
+  // 시작과 끝 인덱스를 이용하여 문자열의 크기를 계산
+  int str_size = end - start;
+  // 슬라이싱된 문자열을 저장할 포인터 변수를 동적 할당
+  char* slice_str = (char*)malloc(sizeof(char) * str_size);
 
-    // 슬라이싱된 문자열을 생성
-    for (int i = 0; i < str_size; i++) {
-      slice_str[i] = s[start + i];
-    }
+  // 슬라이싱된 문자열을 생성
+  for (int i = 0; i < str_size; i++) {
+    slice_str[i] = s[start + i];
+  }
 
-    // 슬라이싱된 문자열을 반환
-    return slice_str;
+  // 슬라이싱된 문자열을 반환
+  return slice_str;
 }
